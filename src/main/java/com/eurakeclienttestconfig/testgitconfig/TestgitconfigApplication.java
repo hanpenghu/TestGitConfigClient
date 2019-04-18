46aefc690cedaf0d877800fc4cd7c4f4d5ca21b0
563909408@qq.com <563909408@qq.com>
2019-04-19 01:15:14 +0800

    Initial commit 2019_4_19   weekday(5)   1:15:11

---
+++ src/main/java/com/eurakeclienttestconfig/testgitconfig/TestgitconfigApplication.java
@@ -0,0 +1,35 @@
+package com.eurakeclienttestconfig.testgitconfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+//自动感知变量更新
+@RefreshScope
+public class TestgitconfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestgitconfigApplication.class, args);
+    }
+
+
+
+
+
+    @Value("${hello}")
+    String hello;
+    @RequestMapping(value = "/hello")
+    public String hello(){
+        return hello;
+    }
+
+
+
+
+
+}
---
+++ src/test/java/com/eurakeclienttestconfig/testgitconfig/TestgitconfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eurakeclienttestconfig.testgitconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestgitconfigApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
